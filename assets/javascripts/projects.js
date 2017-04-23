6b1cab65e4c3890a1212f904d05a7f783b2b76ba
benoit <benoitleo.maillard@gmail.com>
2017-04-23 21:45:41 +0200

    Added dynamic display and json loading via ajax

--- assets/javascripts/projects.js
+++ assets/javascripts/projects.js
@@ -1,75 +1,87 @@
 $(function() {
 	
 
-	var content = [
-		{
-			"title" : "Projet test",
-			"thumb" : "./assets/images/projects/min_SH.png",
-			"items" : [
-				{
-					"content" : "./assets/images/creations/loup_vector.png",
-					"caption" : "Petit test",
-					"type" : "image"
-				},
-				{
-					"content" : "./assets/images/creations/r2d2_v1.png",
-					"caption" : "Petit test 2",
-					"type" : "image"
-				}
-			]
-		}
-	];
+	var content = [];
+
+	var projectIndex = -1;
+	var itemIndex = -1;
 
 	$('.project-thumb').click(function() {
-		console.log(this);
+		
 		$('.lightbox').addClass('show');
 		$('body').addClass('lightbox-fullscreen');
+
+		projectIndex = $(this).attr('data-index');
+		itemIndex = 0;
+
+		$('.img-container').empty();
+		$('.img-container').append(content[projectIndex].items[itemIndex].content);
 	});
 
 	$('.lightbox-close').click(function() {
-		console.log($(this).parent());
+		
 		$(".lightbox").removeClass('show');
 		$('body').removeClass('lightbox-fullscreen');
+
+		projectIndex = -1;
+		itemIndex = -1;
 	});
 
-	/*
+	$('.btn-previous').click(function() {
+		if (itemIndex > 0) {
+			itemIndex -= 1;
+
+			$('.img-container').empty();
+			$('.img-container').append(content[projectIndex].items[itemIndex].content);
+		}
+	});
+
+	$('.btn-next').click(function() {
+		if (itemIndex < content[projectIndex].items.length - 1) {
+			itemIndex += 1;
+
+			$('.img-container').empty();
+			$('.img-container').append(content[projectIndex].items[itemIndex].content);
+		}
+	});
+
+	
 	$.getJSON("./assets/json/content.json", function(data) {
-		var content = data;
+		content = data;
 		console.log("test");
-		*/
+		
 
-	for (var p = 0; p < content.length; p++) {
-		var project = content[p];
+		for (var p = 0; p < content.length; p++) {
+			var project = content[p];
 
-		/*project.thumb = $("<img>").attr('src', project.thumb);*/
+			/*project.thumb = $("<img>").attr('src', project.thumb);*/
 
-		for (var i = 0; i < project.items.length; i++) {
-			var item = project.items[i];
+			for (var i = 0; i < project.items.length; i++) {
+				var item = project.items[i];
 
-			if (item.type == "image") {
-				item.content = $("<img>").attr('src', item.content); 
-			}
+				if (item.type == "image") {
+					item.content = $("<img>").attr('src', item.content); 
+				}
 
-			if (item.type == "vimeo") {
-				item.content = $("<iframe>");
-			}
+				if (item.type == "vimeo") {
+					item.content = $("<iframe>");
+				}
 
-			if (item.type == "youtube") {
-				item.content = $("<iframe>");
+				if (item.type == "youtube") {
+					item.content = $("<iframe>");
+				}
 			}
 		}
-	}
 
-	console.log(content);
+		console.log(content);
 
-	var i = 0;
-	$(".project-container").each(function() {
-		$(this).find('.project-thumb').attr('src', content[i].thumb);
-		i++;
+		var i = 0;
+		$(".project-container").each(function() {
+			$(this).find('.project-thumb').attr('src', content[i].thumb);
+			$(this).find('.project-caption').text(content[i].title);
+			i++;
+		});
 	});
-	
-		/*
-	});*/
 
 	
 
