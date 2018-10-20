ff1f70885216bcade0dbd079dc6afbebf791b550
Mikaël <ruffieux.mikael@gmail.com>
2018-10-20 19:50:28 +0200

    testing contact form

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 $valid = false;
 $sent_to_email = "ruffieux.mikael@gmail.com";
-$order_url = "/redox/index.php";
+$order_url = "/redox/index.html";
 
 if (isset($_POST["contact"])) {
   $text_fields = ["name", "subject"];
