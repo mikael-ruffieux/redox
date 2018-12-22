d6d2df5d2bd1926050e711fc0c0e3725d7263a52
Mikaël Ruffieux <ruffieux.mikael@gmail.com>
2018-12-22 21:03:28 +0100

    form debug 3

--- index.php
+++ index.php
@@ -292,28 +292,28 @@
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <input class="form-control" name="name" type="text" maxlength="60" size="30" placeholder="Votre nom *" required="required" data-validation-required-message="Merci d'entrer votre nom.">
+                    <input class="form-control" id="name" name="name" type="text" maxlength="60" size="30" placeholder="Votre nom *" required="required" data-validation-required-message="Merci d'entrer votre nom.">
                     <p class="help-block text-warning"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" name="email" type="email" maxlength="60" size="30" placeholder="Votre Email *" required="required" data-validation-required-message="Merci d'entrer votre adresse e-mail.">
+                    <input class="form-control" id="email" name="email" type="email" maxlength="60" size="30" placeholder="Votre Email *" required="required" data-validation-required-message="Merci d'entrer votre adresse e-mail.">
                     <p class="help-block text-warning"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" name="phone" type="tel" maxlength="13" placeholder="Votre n° de téléphone">
+                    <input class="form-control" name="phone" id="phone" type="tel" maxlength="13" placeholder="Votre n° de téléphone">
                     <p class="help-block text-warning"></p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-group">
-                    <textarea class="form-control" name="message" placeholder="Votre message *" required="required" data-validation-required-message="Merci d'entrer un message."></textarea>
+                    <textarea id="message" class="form-control" name="message" placeholder="Votre message *" required="required" data-validation-required-message="Merci d'entrer un message."></textarea>
                     <p class="help-block text-warning"></p>
                   </div>
                 </div>
                 <div class="clearfix"></div>
                 <div class="col-lg-12 text-center">
                   <div id="success"></div>
-                  <input name="contact" class="btn btn-primary btn-xl text-uppercase" type="submit" value="Envoyer" name="contact">
+                  <input id="contact" class="btn btn-primary btn-xl text-uppercase" type="submit" value="Envoyer" name="contact">
                 </div>
               </div>
             </form>
