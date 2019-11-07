5aa72a3d6396a2e13184f544bf533eb320edb1aa
Mikaël Ruffieux <ruffieux.mikael@gmail.com>
2019-11-07 19:21:53 +0100

    mise en place du layout du blog

--- blog/all-posts.php
+++ blog/all-posts.php
@@ -9,17 +9,14 @@
   <?php include 'layout/navbar.html'; ?>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+  <header class="masthead" style="background-image: ;">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="post-heading">
-            <h1>Man must explore, and this is exploration at its greatest</h1>
-            <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
-            <span class="meta">Posted by
-              <a href="#">Start Bootstrap</a>
-              on August 24, 2019</span>
+          <div class="site-heading">
+            <h1>Tous les articles</h1>
+            <!--<span class="subheading">Le blog de Mikaël - un aperçu de mon quotidien</span>-->
           </div>
         </div>
       </div>
@@ -31,7 +28,7 @@
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <div class="post-preview">
-          <a href="posts.html">
+          <a href="/blog/posts/speak-up">
             <h2 class="post-title">
               SpeakUp, la nouvelle plateforme de diffusion de Podcasts
             </h2>
@@ -40,12 +37,12 @@
             </h3>
           </a>
           <p class="post-meta">Posté par
-            <a href="about.html">Mikaël</a>
+            <a href="/blog/about">Mikaël</a>
             le 1 décembre 2019</p>
         </div>
         <hr>
         <div class="post-preview">
-          <a href="posts.html">
+          <a href="/blog/posts/speak-up">
             <h2 class="post-title">
               Créer un blog ? Mais je n'ai rien à dire !
             </h2>
@@ -54,13 +51,8 @@
             </h3>
           </a>
           <p class="post-meta">Posté par
-            <a href="about.html">Mikaël</a>
+            <a href="/blog/about">Mikaël</a>
             le 11 novembre 2019</p>
-        </div>
-        <hr>
-        <!-- Pager -->
-        <div class="clearfix">
-          <a class="btn btn-primary float-right" href="posts.html">Tous les articles &rarr;</a>
         </div>
       </div>
     </div>
--- blog/contact.php
+++ blog/contact.php
@@ -35,28 +35,28 @@
           <div class="control-group">
             <div class="form-group floating-label-form-group controls">
               <label>Nom</label>
-              <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Merci d'entrer votre nom.">
+              <input type="text" class="form-control" placeholder="Nom" id="name" required data-validation-required-message="Merci d'entrer votre nom.">
               <p class="help-block text-danger"></p>
             </div>
           </div>
           <div class="control-group">
             <div class="form-group floating-label-form-group controls">
               <label>Adresse e-mail</label>
-              <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Merci d'entrer votre e-mail.">
+              <input type="email" class="form-control" placeholder="Adresse e-mail" id="email" required data-validation-required-message="Merci d'entrer votre e-mail.">
               <p class="help-block text-danger"></p>
             </div>
           </div>
           <div class="control-group">
             <div class="form-group col-xs-12 floating-label-form-group controls">
               <label>Numéro de téléphone</label>
-              <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Merci d'entrer votre numéro de téléphone.">
+              <input type="tel" class="form-control" placeholder="Numéro de téléphone" id="phone" required data-validation-required-message="Merci d'entrer votre numéro de téléphone.">
               <p class="help-block text-danger"></p>
             </div>
           </div>
           <div class="control-group">
             <div class="form-group floating-label-form-group controls">
-              <label>Message</label>
-              <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Merci d'entrer un message."></textarea>
+              <label>Votre message</label>
+              <textarea rows="5" class="form-control" placeholder="Votre message" id="message" required data-validation-required-message="Merci d'entrer un message."></textarea>
               <p class="help-block text-danger"></p>
             </div>
           </div>
